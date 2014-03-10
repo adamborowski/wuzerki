ed4869aceb02111b02a572babb789b1832e4a2bc
pawelc222 <pawelc222@gmail.com>
2014-03-10 12:25:57 +0100

    porawa serwer

--- lab1/wzrServer/wzrServer/wzrServer.cpp
+++ lab1/wzrServer/wzrServer/wzrServer.cpp
@@ -20,8 +20,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	unicast_net *uni_send = new unicast_net(10002);
 	unsigned long ClientsIp[20] = {};
 	unsigned long ClientIp = 0;
-
-	char frame[88] = {'r','a','m','k','a'};
+	unsigned long ClientsTimeout[20] = {};
+	unsigned int timeout = 1000;
+	char frame[88] = {};
 	char* message = frame;
 	int rozmiar;                             // liczba bajtów ramki otrzymanej z sieci
 	
@@ -35,20 +36,35 @@ int _tmain(int argc, _TCHAR* argv[])
 			if(ClientsIp[i] == 0)
 			{
 				ClientsIp[i] = ClientIp;
-				std::cout << "Connected client, IP: " << ClientIp;
+				ClientsTimeout[i] = timeout;
+				std::cout << "Connected client, IP: " << ClientIp << std::endl;
 			}
 			if(ClientsIp[i] == ClientIp)
+			{
+				ClientsTimeout[i]=timeout;
 				break;			
+			}
 		}
 		for(int i=0; i<20; i++)
 		{
 			if(ClientsIp[i] != 0 && ClientsIp[i] != ClientIp)
 			{
 				//(char*)&ramka, SERVER_IP, sizeof(Ramka)
+				//if(ClientsTimeout[i] > 0)
 				uni_send->send(frame, ClientsIp[i], FrameSize);
-				std::cout << "Data send to client, IP: " << ClientsIp[i];
+				//std::cout << "Data send to client, IP: " << ClientsIp[i] << endl;
 			}
-		}
+			if(ClientsIp[i] != 0 && ClientsTimeout[i]>0)
+			{
+				ClientsTimeout[i]--;
+				if(ClientsTimeout[i] <= 0)
+				{
+					std::cout << "Disconnected client, IP: " << ClientIp << std::endl;
+					ClientsIp[i]=0;
+				}
+			}
+			
+		}		
 
 	}  
 	return 0;
